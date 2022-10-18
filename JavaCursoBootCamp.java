cfa3a77a6a46d5a00be2d7e7e6ef82e0cb235737
FabricioGarciaGitHub <116042028+FabricioGarciaGitHub@users.noreply.github.com>
2022-10-17 21:03:49 -0300

    Add files via upload

---
+++ JavaCursoBootCamp.java
@@ -0,0 +1,11 @@
+package javacursobootcamp;
+
+public class JavaCursoBootCamp {
+
+	public static void main(String[] args) {
+		System.out.println("Primer mensaje de prueba eclipse GitHub.");
+		System.out.println("Otro mensaje.");
+
+	}
+
+}
